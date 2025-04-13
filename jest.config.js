7328d3469e6c7221f3c08b6617c3c5d53ad78c0e
nzomutchawilfrid <nzomutchawilfrid@gmail.com>
2025-04-13 22:33:59 +0100

    create test environmenert

--- jest.config.js
+++ jest.config.js
@@ -1,21 +1,20 @@
 module.exports = {
-    preset: '@shelf/jest-mongodb',
-    coverageDirectory: './coverage',
-    testEnvironment: 'node',
-    collectCoverageFrom: [
-      'src/**/*.js',
-      '!src/app.js',
-      '!src/server.js',
-      '!src/models/*.js',
-      '!src/tests/**'
-    ],
-    setupFilesAfterEnv: ['./jest.setup.js'],
-    coverageThreshold: {
-      global: {
-        branches: 80,
-        functions: 80,
-        lines: 80,
-        statements: 80
-      }
+  testEnvironment: 'node',
+  coverageDirectory: './coverage',
+  collectCoverageFrom: [
+    'src/**/*.js',
+    '!src/app.js',
+    '!src/server.js',
+    '!src/models/*.js',
+    '!src/tests/**'
+  ],
+  setupFilesAfterEnv: ['./jest.setup.js'],
+  coverageThreshold: {
+    global: {
+      branches: 80,
+      functions: 80,
+      lines: 80,
+      statements: 80
     }
-  };
+  }
+};
--- jest.setup.js
+++ jest.setup.js
@@ -1,2 +1,16 @@
 process.env.MONGO_URL = 'mongodb://localhost:27017/vehicle-test';
 process.env.NODE_ENV = 'test';
+
+const mongoose = require('mongoose');
+
+beforeAll(async () => {
+  const mongoUri = process.env.MONGO_URL;
+  await mongoose.connect(mongoUri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  });
+});
+
+afterAll(async () => {
+  await mongoose.disconnect();
+});
